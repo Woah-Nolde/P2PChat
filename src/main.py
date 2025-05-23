1d7a8000da27ed0d3c469fc058b3dcb2624de735
noahw <noah.wo.uni@gmail.com>
2025-05-23 19:34:47 +0200

    main.py | definiere main funktion

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+from network.messenger import send_msg
+from network.messenger import receive_messages
 
 def send_join(handle, port):
     msg = f"JOIN {handle} {port}" 
@@ -7,5 +9,21 @@ def send_join(handle, port):
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #@brief stelle auf socket Ebene ein, dass an die Broadcast Adresse versendet werden kann
         s.sendto(msg.encode(), ('255.255.255.255', 4000)) #@brief schicke JOIN Nachricht an Broadcast Adresse
 
+send_msg()
+receive_messages()
 
-        
+
+def main():
+    handle = input("Benutzernamen: ") 
+    port = int(input("Port: "))     #@brief Port auf dem man erreichbar ist
+    send_join(handle,port)
+    thread = threading.Thread(target=receive_messages, args=(port,))
+    thread.daemon = True
+    thread.start()
+
+
+    
+    
+
+if __name__ == "__main__":
+    main()
