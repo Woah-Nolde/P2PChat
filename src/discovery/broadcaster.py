9ac442ed1d9b5984bd8128d2ecf1295df7e8874c
noahw <noah.wo.uni@gmail.com>
2025-05-23 10:27:34 +0200

    broadcaster.py | implementiere JOIN

--- src/discovery/broadcaster.py
+++ src/discovery/broadcaster.py
@@ -15,8 +15,27 @@ Bytes = 1024  #@brief maximale groeße in bytes
 @brief im folgenden wird ein UDP socket definiert
 
 """""
-clients = {} 
+clients = {} #@brief liste für alle teilnehmer im Netz
 udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #@brief erstelle einen internet udp socket
 udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #@brief erlaubt wiederverwendung von udp socket (auch belegt)
 udp_sock.bind(('', DISCOVERY_PORT)) #@brief Bindung an den Port 4000 und lauscht auf allen interfaces sei es lan , wlan oder localhost
 print(f"[Discovery] Lausche auf UDP-Port {DISCOVERY_PORT}...") 
+
+while True:
+    data, addr = udp_sock.recvfrom(Bytes) #@brief Empfangen von Nachrichten
+    message = data.decode().strip()  #@brief entfernt leerzeichen am anfang und ender der nachricht
+    print(f"[Discovery] Nachricht erhalten von {addr}: {message}")
+
+    parts = message.split() #@brief nachricht wird decoded und aufegeteilt in hanlde command und Port (alles wird in der liste parts gespeichert)
+    if not parts:  #@brief falls die nachricht leer ist,
+        continue
+
+    command = parts[0] #@brief command wird in einer variable gespeichert (sollte das erste Wort sein)
+
+    if command == "JOIN" and len(parts) == 3: 
+        handle = parts[1]                
+        port = parts[2]         
+        ip = addr[0]              #@brief einzelnen teile der liste 'parts' werden gespeichert
+
+        clients[handle] = (ip, port) #@brief speicher die Sender informationen in 'clients'
+        print(f"[Discovery] {handle} ist online: {ip}:{port}")
