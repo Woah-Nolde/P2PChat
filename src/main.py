15a8de5e9c19d9396eb91b3a7c1376e8f0fe92a7
noahw <noah.wo.uni@gmail.com>
2025-05-26 14:43:04 +0200

    main.py | Ändere Broadcastadresse

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def send_join(handle, port):
     msg = f"JOIN {handle} {port}" 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #@brief stelle auf socket Ebene ein, dass an die Broadcast Adresse versendet werden kann
-        s.sendto(msg.encode(), ('255.255.255.255', 4000)) #@brief schicke JOIN Nachricht an Broadcast Adresse
+        s.sendto(msg.encode(), ('172.21.176.255', 4000)) #@brief schicke JOIN Nachricht an Broadcast Adresse
 
 def send_leave(handle):
     message = f"LEAVE {handle}"
@@ -56,7 +56,7 @@ def discover_users():
 def main():
     handle = input("Benutzernamen: ") 
     port = int(input("Port: "))     #@brief Port auf dem man erreichbar ist
-    send_join(handle,port)
+    send_join(handle, port)
     thread = threading.Thread(target=receive_messages, args=(port,))
     thread.daemon = True
     thread.start()
